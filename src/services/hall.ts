c99eb0e176c26f102f7b55746bb063470cbaa9a7
Siarhei Lunski <the.sergaunt@gmail.com>
2019-03-29 11:45:35 +0300

    create hall with rows

--- src/services/hall.ts
+++ src/services/hall.ts
@@ -1,12 +1,35 @@
-import Hall from '../models/Hall';
+import sequilize from '../config/sequelize';
 
-import { HallType } from '../types/hall';
+import Hall from '../models/Hall';
+import Row from '../models/Row';
+
+import { HallType, RowType } from '../types/hall';
 
 export default {
   async create(body: HallType): Promise<boolean> {
-    console.log(body);
-    // await Hall.create(body);
-    return true;
+    const hallBody = {
+      title: body.title,
+      'cinema-id': body['cinema-id']
+    };
+
+    const transaction = await sequilize.transaction();
+    try {
+      const hall = await Hall.create(hallBody, {
+        transaction,
+        returning: true
+      });
+      const rowsArrBody = body.rows.map(row => ({
+        ...row,
+        'category-id': row.categoryID,
+        'hall-id': hall.id
+      }));
+      await Row.bulkCreate(rowsArrBody, { transaction, validate: true });
+      await transaction.commit();
+      return true;
+    } catch (error) {
+      if (error) await transaction.rollback();
+      return false;
+    }
   },
 
   async getAll(): Promise<HallType[]> {
--- src/types/hall.ts
+++ src/types/hall.ts
@@ -1,6 +1,17 @@
 export interface HallType {
   id?: number;
   title: string;
+  'cinema-id': number;
+  rows: RowType[];
   createdAt?: Date;
   updatedAt?: Date;
+}
+
+export interface RowType {
+  categoryID: number;
+  price: number;
+  seats: number;
+  reserved: number[];
+  ordered: number[];
+  lastInSection: boolean;
 }
