01659eb191c1a01d30aebb698b10e7fb5616ed42
Siarhei Lunski <the.sergaunt@gmail.com>
2019-04-13 18:43:38 +0300

    define production environment

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -3,20 +3,36 @@ import { env } from '../config/env';
 
 const SequelizeModule: any = Sequelize;
 
-const sequelize = new SequelizeModule({
-  dialect: 'postgres',
-  host: env.DB_HOST,
-  database: env.DB_NAME,
-  username: env.DB_USER,
-  password: env.DB_PASSWORD,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  logging: false
-});
+const config =
+  process.env.NODE_ENV === 'production'
+    ? {
+        dialect: 'postgres',
+        host: `/cloudsql/${env.CLOUD_SQL_CONNECTION_NAME}`,
+        user: env.DB_USER,
+        password: env.DB_PASSWORD,
+        database: env.DB_NAME,
+        pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000
+        }
+      }
+    : {
+        dialect: 'postgres',
+        host: env.DB_HOST,
+        database: env.DB_NAME,
+        username: env.DB_USER,
+        password: env.DB_PASSWORD,
+        pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000
+        }
+      };
+
+const sequelize = new SequelizeModule(config);
 
 sequelize.sync();
 
